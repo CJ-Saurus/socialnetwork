538183d639bea043f29a7555d02c6ca416a39a18
Christopher <christopherperez@Christophers-Laptop.local>
2023-06-29 17:46:07 -0500

    troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetwork', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetwork/SocialNetwork', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
